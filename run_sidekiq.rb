9dd436b69d3d16bab7bc5ab08c6dd48dad9f36f9
Tyler Fink <tfink419@gmail.com>
2022-09-26 20:44:26 -0600

    Send 10 events for next 10 minutes

--- run_sidekiq.rb
+++ run_sidekiq.rb
@@ -1,4 +1,15 @@
 require 'sidekiq'
 require './garage_status_worker'
 
-GarageStatusWorker.perform_async((ENV['TIMER_INTERVAL'] || 300).to_i)
+INTERVAL = (ENV['TIMER_INTERVAL'] || 300).to_i
+
+GarageStatusWorker.perform_async(INTERVAL)
+GarageStatusWorker.perform_at(Time.now+60, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+120, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+180, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+240, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+300, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+360, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+420, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+480, INTERVAL)
+GarageStatusWorker.perform_at(Time.now+540, INTERVAL)
